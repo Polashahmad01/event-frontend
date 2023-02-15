9e8addc0b75d720b353acbe3362256b0f8935039
Polashahmad01 <dev.polashahmad@gmail.com>
2023-02-15 16:48:50 +0600

    add basic form for creating new event

---
+++ src/components/form/TextField.jsx
@@ -0,0 +1,23 @@
+import { TextField } from "@mui/material"
+import { useField } from "formik"
+
+export const TextFieldWrapper = ({ name, ...otherProps }) => {
+  const [field, meta] = useField(name)
+
+  const configTextField = {
+    name,
+    ...otherProps,
+    ...field,
+    fullWidth: true,
+    variant: "outlined"
+  }
+
+  if(meta && meta.touched && meta.error) {
+    configTextField.error = true
+    configTextField.helperText = meta.error
+  }
+
+  return (
+    <TextField {...configTextField} />
+  )
+}
---
+++ src/pages/Event/CreateEvent/components/hooks/useCreateNewEventForm.js
@@ -0,0 +1,37 @@
+import * as Yup from "yup"
+
+const INITIAL_FORM_STATE = {
+  title: "",
+  author: "",
+  contentType: "",
+  tag: "",
+  contentUrl: "",
+  summary: "",
+  description: ""
+}
+
+const FORM_VALIDATION = Yup.object().shape({
+  title: Yup.string()
+    .min(5)
+    .required("title is required"),
+  author: Yup.string()
+    .required("author is required"),
+  contentType: Yup.string()
+    .required('content type is required'),
+  tag: Yup.string()
+    .required("tag is required"),
+  contentUrl: Yup.string()
+    .required('content url is required'),
+  summary: Yup.string()
+    .required("summary is required"),
+  description: Yup.string()
+    .required("description is required")
+})
+
+export const useCreateNewEventForm = () => {
+
+  return {
+    INITIAL_FORM_STATE,
+    FORM_VALIDATION
+  }
+}
