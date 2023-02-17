9feaee531c27d0ea0357c3793d1a0f3c36b7d413
Polashahmad01 <dev.polashahmad@gmail.com>
2023-02-17 22:50:55 +0600

    add toast notification for the whole app

--- src/layout/RootAppLayout.jsx
+++ src/layout/RootAppLayout.jsx
@@ -1,7 +1,10 @@
 import { Outlet } from "react-router"
 import { Box, Container } from "@mui/material"
+import { ToastContainer } from "react-toastify"
 
 import { RootAppNavbar } from "../components/RootAppNavbar"
+
+import "react-toastify/dist/ReactToastify.css"
 
 export const RootAppLayout = () => {
 
@@ -11,6 +14,17 @@ export const RootAppLayout = () => {
       <Box>
         <Container>
           <Outlet />
+          <ToastContainer
+            position="top-right"
+            autoClose={1500}
+            hideProgressBar
+            newestOnTop
+            closeOnClick
+            rtl={false}
+            pauseOnFocusLoss
+            draggable
+            pauseOnHover
+          />
         </Container>
       </Box>
     </>
