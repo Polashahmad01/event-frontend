46da01ece3d7079e22c4108286558da07daa5a31
Polashahmad01 <dev.polashahmad@gmail.com>
2023-02-17 19:14:48 +0600

    user can add tag

--- src/pages/Tag/components/hooks/useCreateTagForm.js
+++ src/pages/Tag/components/hooks/useCreateTagForm.js
@@ -15,8 +15,35 @@ const FORM_VALIDATION = Yup.object().shape({
 })
 
 export const useCreateTagForm = () => {
-  const tagsFormHandler = (values, actions) => {
-    console.log(values)
+
+  const postTagData = async (data) => {
+    const response = await fetch('https://event-backend.vercel.app/api/v1/tags', {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify(data)
+    })
+
+    if(!response.ok) {
+      throw new Error(`request error ${method} https://event-backend.vercel.app/api/v1/tags statusCode: ${response.status} statusText: ${response.statusText}`)
+    }
+    
+    const result = await response.json()
+    return result
+  }
+
+  const tagsFormHandler = async (values, actions) => {
+    const formData = {
+      tagName: values.tagName,
+      tagDescription: values.tagDescription
+    }
+
+    const data = await postTagData(formData)
+
+    alert(data.data)
+
+    console.log(data)
   }
 
   return {
