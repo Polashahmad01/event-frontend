eb66551954507883d68f9040147b0139727b3b11
Polashahmad01 <dev.polashahmad@gmail.com>
2023-02-19 15:22:06 +0600

    tags is loading from the server

--- src/lib/http/EventHttpClient.js
+++ src/lib/http/EventHttpClient.js
@@ -3,6 +3,7 @@ import { HttpClient } from "./HttpClient"
 export class EventHttpClient {
   client = new HttpClient()
 
-  createNewEvent = (eventFormData) => this.client.post("api/v1/event", eventFormData)
+  createNewEvent = async (eventFormData) => await this.client.post("api/v1/event", eventFormData)
   
+  getAllTags = async () => await this.client.get("api/v1/tags")
 }
